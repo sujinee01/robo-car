c715d6dc19d73a57eaf13e466ace0e2ac90e029d
Heejong Lee <frogrammer.hjong@gmail.com>
2023-10-27 11:43:56 +0900

    fix: 회원상세조회 [목록] 버튼 동작 복구

--- src/components/Manager_member.js
+++ src/components/Manager_member.js
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import styles from "../style/Manager_member.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import ManagerHeader from "./Manager_header";
+import MemberDetail from "./Manager_member_detail";
+// import ManagerHeader from "./Manager_header";
 
 function ManagerMember({ userData }) {
   let users = [
@@ -18,7 +20,14 @@ function ManagerMember({ userData }) {
   if (userData) {
     users = userData;
   }
-  // console.log(userData[0]);
+
+  const [searchId, setSearchId] = useState("");
+  const [detailToggle, setDetailToggle] = useState(false);
+
+  const handleDetail = (searchId) => {
+    setDetailToggle(!detailToggle);
+    setSearchId(searchId);
+  };
 
   function User({ user, key }) {
     return (
@@ -38,7 +47,7 @@ function ManagerMember({ userData }) {
         </td>
         <td>
           {/*상세조회버튼*/}
-          <button>
+          <button onClick={() => handleDetail(user.u_id)}>
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
               className={styles.faMagnifyingGlass}
@@ -51,29 +60,33 @@ function ManagerMember({ userData }) {
 
   return (
     <>
-      <ManagerHeader />
-      <div className={styles.member_wrap}>
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>이름</th>
-              <th>아이디</th>
-              <th>휴대전화</th>
-              <th>이메일</th>
-              <th>주소</th>
-              <th>회사</th>
-              <th>삭제</th>
-              <th>조회</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <User user={user} key={index} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {/* <ManagerHeader /> */}
+      {detailToggle ? (
+        <MemberDetail handleDetail={handleDetail} searchId={searchId} />
+      ) : (
+        <div className={styles.member_wrap}>
+          <table>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>이름</th>
+                <th>아이디</th>
+                <th>휴대전화</th>
+                <th>이메일</th>
+                <th>주소</th>
+                <th>회사</th>
+                <th>삭제</th>
+                <th>조회</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <User user={user} key={index} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 }
