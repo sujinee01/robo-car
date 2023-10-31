557f2f3d5d956eca499fa6312533fe5b52e49a39
Heejong Lee <frogrammer.hjong@gmail.com>
2023-10-31 15:18:57 +0900

    feat: [공지사항관리] 삭제버튼 기능 추가

--- src/components/Manager_notice.js
+++ src/components/Manager_notice.js
@@ -1,27 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../style/Manager_notice.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import NoticeAdd from "./Manager_notice_add";
 
 function ManagerNotice({ noticeData }) {
-  let notices = [
-    {
-      nb_idx: 0,
-      nb_title: "",
-      nb_auth: "",
-      nb_important: "",
-    },
-  ];
-  if (noticeData) {
-    notices = noticeData;
-  }
-
   const [addToggle, setAddToggle] = useState(false);
+  const [receiveData, setReceiveData] = useState([]);
 
   const handleAdd = () => {
     setAddToggle(!addToggle);
   };
+
+  const infoReq = async () => {
+    const reqTarget = "Notice";
+
+    try {
+      const response = await fetch("/ManageMember", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ reqTarget }), // 요청하는 데이터
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        if (data.success) {
+          setReceiveData(data["rows"]);
+        }
+      }
+    } catch (error) {
+      console.error("오류:", error);
+    }
+  };
+
+  const delNotice = async (noti) => {
+    const reqTarget = "delNotice";
+    const notiIdx = noti;
+
+    try {
+      const response = await fetch("/ManageMember", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ reqTarget, notiIdx }), // 요청하는 데이터
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        if (data.success) {
+          const updatedData = receiveData.filter(
+            (notice) => notice.nb_idx !== notiIdx
+          );
+          setReceiveData(updatedData);
+        }
+      }
+    } catch (error) {
+      console.error("오류:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (addToggle === false) {
+      infoReq();
+    }
+  }, [addToggle]);
+
+  useEffect(() => {
+    console.log(">>> ", addToggle);
+    if (addToggle === false) {
+      infoReq();
+    }
+  }, []);
 
   function Notice({ notice, key }) {
     return (
@@ -42,7 +96,7 @@ function ManagerNotice({ noticeData }) {
         </td>
         <td>
           {/*삭제버튼*/}
-          <button>
+          <button onClick={() => delNotice(notice.nb_idx)}>
             <FontAwesomeIcon icon={faXmark} className={styles.faXmark} />
           </button>
         </td>
@@ -56,23 +110,59 @@ function ManagerNotice({ noticeData }) {
         <NoticeAdd handleAdd={handleAdd} />
       ) : (
         <div className={styles.notice_wrap}>
-          <table>
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>중요</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>수정</th>
-                <th>삭제</th>
-              </tr>
-            </thead>
-            <tbody>
-              {notices.map((notice, index) => (
-                <Notice notice={notice} key={index} />
-              ))}
-            </tbody>
-          </table>
+          {receiveData ? (
+            receiveData.length === 0 ? (
+              <>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>No.</th>
+                      <th>중요</th>
+                      <th>제목</th>
+                      <th>작성자</th>
+                      <th>수정</th>
+                      <th>삭제</th>
+                    </tr>
+                  </thead>
+                </table>
+                <p>공지사항이 없습니다.</p>
+              </>
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>중요</th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                    <th>수정</th>
+                    <th>삭제</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {receiveData.map((notice, index) => (
+                    <Notice notice={notice} key={index} />
+                  ))}
+                </tbody>
+              </table>
+            )
+          ) : (
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>중요</th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                    <th>수정</th>
+                    <th>삭제</th>
+                  </tr>
+                </thead>
+              </table>
+              <p>공지사항이 없습니다.</p>
+            </>
+          )}
           <div className={styles.notice_add}>
             <button onClick={handleAdd}>공지사항 등록</button>
           </div>
